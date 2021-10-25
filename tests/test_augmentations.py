8d8446f4bafbf5327802522e7e573b4c301176cb
Luca Actis Grosso <luca.actisgrosso@generali.com>
2021-10-25 14:24:44 +0200

    fix test augmentation

--- tests/test_augmentations.py
+++ tests/test_augmentations.py
@@ -6,18 +6,18 @@ import torch
 def test_get_random_index():
     torch.manual_seed(0)
     x = torch.Tensor([1, 2, 3])
-    expected = torch.from_numpy(np.array([2, 0, 1]))
-    actual = get_random_index(x)
-    torch.testing.assert_equal(actual, expected)
+    expected = np.array([2, 0, 1])
+    actual = get_random_index(x).numpy()
+    np.testing.assert_array_equal(actual, expected)
 
 
 def test_mixup():
     x = torch.Tensor([[1, 1], [2, 2], [3, 3]])
     lam = 0.5
     random_index = torch.from_numpy(np.array([2, 0, 1]))
-    expected = torch.Tensor([[2.0, 2.0], [1.5, 1.5], [2.5, 2.5]])
-    actual = mixup(x=x, random_index=random_index, lam=lam)
-    torch.testing.assert_equal(actual, expected)
+    expected = torch.Tensor([[2.0, 2.0], [1.5, 1.5], [2.5, 2.5]]).numpy()
+    actual = mixup(x=x, random_index=random_index, lam=lam).numpy()
+    np.testing.assert_array_equal(actual, expected)
 
 
 def test_cutmix():
@@ -25,6 +25,6 @@ def test_cutmix():
     x = torch.Tensor([[1, 1], [2, 2], [3, 3]])
     lam = 0.5
     random_index = torch.from_numpy(np.array([2, 0, 1]))
-    expected = torch.Tensor([[1.0, 1.0], [2.0, 2.0], [2.0, 3.0]])
-    actual = cutmix(x=x, random_index=random_index, lam=lam)
-    torch.testing.assert_equal(actual, expected)
+    expected = torch.Tensor([[1.0, 1.0], [2.0, 2.0], [2.0, 3.0]]).numpy()
+    actual = cutmix(x=x, random_index=random_index, lam=lam).numpy()
+    np.testing.assert_array_equal(actual, expected)
